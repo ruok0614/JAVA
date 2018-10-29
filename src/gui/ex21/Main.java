82ed5a7da76ec2f36159cd70d30eba96e3dde899
DESKTOP-M1OSMU9\qazws <qazwsxedc0614@gmail.com>
2018-10-29 22:27:14 +0900

    gui課題2-1を追加

---
+++ src/gui/ex21/Main.java
@@ -0,0 +1,9 @@
+package gui.ex21;
+
+public class Main {
+    public static void main(String args[]){
+        ClockFrame clockFrame = new ClockFrame();
+        clockFrame.setVisible(true);
+
+    }
+}
